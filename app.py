dd19e33d2cb24b284a7ea3d384565f0fb0692a38
Manow99 <89176611+Manow99@users.noreply.github.com>
2021-12-27 16:40:58 +0700

    Update app.py

--- app.py
+++ app.py
@@ -159,9 +159,9 @@ def event_handle(event,json_line):
             replyObj = TextSendMessage(text="ไปดิ")
             line_bot_api.reply_message(rtoken, replyObj)
         elif msg == "covid":
-            url = "https://covid19.ddc.moph.go.th/api/Cases/today-cases-all"
-            response = requests.get(url)
-            response = response.json()
+            #url = "https://covid19.ddc.moph.go.th/api/Cases/today-cases-all"
+            #response = requests.get(url)
+            #response = response.json()
             replyObj = TextSendMessage(text="ไปดิ")
             line_bot_api.reply_message(rtoken, replyObj)
         elif msg == "Covid":
